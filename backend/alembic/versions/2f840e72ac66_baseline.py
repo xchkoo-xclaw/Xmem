50dfeec8295c679f9a27de0aff3728e04fa182a3
Xchkoo <Xchkoo@foxmail.com>
2026-02-02 12:31:09 +0800

    fix alembic bugs

--- backend/alembic/versions/2f840e72ac66_baseline.py
+++ backend/alembic/versions/2f840e72ac66_baseline.py
@@ -5,17 +5,11 @@ Revises: None
 Create Date: 2026-02-02 00:00:00.000000
 
 """
-from typing import Sequence, Union
-
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = "2f840e72ac66"
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = "2f840e72ac66"
+down_revision = None
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
